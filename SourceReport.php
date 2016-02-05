c28a301b3744acb7038d92429c44844a69ebc5db
Daniel Kocevski <dankocevski@gmail.com>
2016-02-05 13:16:14 -0500

    Adding a new php header

--- SourceReport.php
+++ SourceReport.php
@@ -1,5 +1,5 @@
 <?php
-header('Content-type: text/html; charset=UTF-8');
+	header('Content-Type:text/html; charset=UTF-8');
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en-us">
