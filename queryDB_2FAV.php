f9f96ca01b265f4942330f37cc2c68a747fe4815
Daniel Kocevski <dankocevski@gmail.com>
2018-05-07 16:40:33 -0500

    Changing from the flare sqlite database to the mysql database

--- queryDB_2FAV.php
+++ queryDB_2FAV.php
@@ -36,8 +36,28 @@
 
     }
 
+    // Setup the databa info
+    $servername = "asddb.gsfc.nasa.gov";
+    $username = "favaread";
+    $password = "IhopeFAVAdataworks";
+
     // Initiate the database connection
-    $db = new SQLite3 ('./db/fava_flares.db');
+    $conn = mysql_connect($servername, $username, $password);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+    // echo "Connected successfully";
+
+
+    // echo "<BR><BR>type of request<BR>";
+    // echo $typeOfRequest;
+    // echo "<BR><BR>";
+
+
+    // Select the database
+    mysql_select_db('FAVA');
 
     // Return the 2FAV catalog (2933 flares)
     if ($typeOfRequest === '2FAV') { 
@@ -45,31 +65,39 @@
         $and = ' and ';
         $or = ' or ';
 
-        $cut1 = '((cast(week as float) < 340) and (cast(he_sigma as float)>6) and (cast(sundist as float)>10))';
-        $cut2 = '((cast(week as float) < 340) and (cast(sigma as float)>6) and (cast(sundist as float)>10))';
-        $cut3 = '((cast(week as float) < 340) and ((cast(sigma as float)>4) and (cast(he_sigma as float)>4) and (cast(sundist as float)>10)) and ((cast(sigma as float)<=6) or (cast(he_sigma as float)<=6)))';
+        $cut1 = '((week < 340) and (he_sigma>6) and (sundist>10))';
+        $cut2 = '((week < 340) and (sigma>6) and (sundist>10))';
+        $cut3 = '((week < 340) and ((sigma>4) and (he_sigma>4) and (sundist>10)) and ((sigma<=6) or (he_sigma<=6)))';
 
-        $cut4 = '((cast(week as float) < 340) and ((cast(he_ts as float)>39) and (cast(he_sundist as float)>10)) and ((cast(he_contflag as float)==0) or (cast(he_contflag as float)==1)))';
-        $cut5 = '((cast(week as float) < 340) and ((cast(le_ts as float)>39) and (cast(le_sundist as float)>10)) and ((cast(le_contflag as float)==0) or (cast(le_contflag as float)==1)))';
-        $cut6 = '((cast(week as float) < 340) and ((cast(le_ts as float)>18) and (cast(he_ts as float)>18)) and ((cast(le_sundist as float)>10) and (cast(he_sundist as float)>10)) and ((cast(le_contflag as float)==0) or (cast(le_contflag as float)==1)) and ((cast(he_contflag as float)==0) or (cast(he_contflag as float)==1)) and (cast(he_le_dist as float)<1.5) and ((cast(he_ts as float)<=39) or (cast(le_ts as float)<=39)))';
+        $cut4 = '((week < 340) and ((he_ts>39) and (he_sundist>10)) and ((he_contflag=0) or (he_contflag=1)))';
+        $cut5 = '((week < 340) and ((le_ts>39) and (le_sundist>10)) and ((le_contflag=0) or (le_contflag=1)))';
+        $cut6 = '((week < 340) and ((le_ts>18) and (he_ts>18)) and ((le_sundist>10) and (he_sundist>10)) and ((le_contflag=0) or (le_contflag=1)) and ((he_contflag=0) or (he_contflag=1)) and (he_le_dist<1.5) and ((he_ts<=39) or (le_ts<=39)))';
 
-        $queryStatement = 'SELECT flareID, cast(num as int), best_ra, best_dec, best_r95, bestPositionSource, fava_ra, fava_dec, lbin, bbin, gall, galb, tmin, tmax, sigma, avnev, nev, he_nev, he_avnev, he_sigma, sundist, varindex, favasrc, fglassoc, assoc, le_ts, le_tssigma, le_ra, le_dec, le_gall, le_galb, le_r95, le_contflag, le_sundist, le_dist2bb, le_ffsigma, le_hightsfrac, le_gtlts, le_flux, le_fuxerr, le_index, le_indexerr, he_ts, he_tssigma, he_ra, he_dec, he_gall, he_galb, he_r95, he_contflag, he_sundist, he_dist2bb, he_ffsigma, he_hightsfrac, he_le_dist, he_gtlts, he_flux, he_fuxerr, he_index, he_indexerr, week, dateStart, dateStop from flares WHERE ' . $cut1 . $or . $cut2 . $or . $cut3 . $or . $cut4 . $or . $cut5 . $or . $cut6 . ' ORDER BY cast(best_ra as float) ASC';
+        $queryStatement = 'SELECT flareID, num, best_ra, best_dec, best_r95, bestPositionSource, fava_ra, fava_dec, lbin, bbin, gall, galb, tmin, tmax, sigma, avnev, nev, he_nev, he_avnev, he_sigma, sundist, varindex, favasrc, fglassoc, assoc, le_ts, le_tssigma, le_ra, le_dec, le_gall, le_galb, le_r95, le_contflag, le_sundist, le_dist2bb, le_ffsigma, le_hightsfrac, le_gtlts, le_flux, le_fuxerr, le_index, le_indexerr, he_ts, he_tssigma, he_ra, he_dec, he_gall, he_galb, he_r95, he_contflag, he_sundist, he_dist2bb, he_ffsigma, he_hightsfrac, he_le_dist, he_gtlts, he_flux, he_fuxerr, he_index, he_indexerr, week, dateStart, dateStop from flares WHERE ' . $cut1 . $or . $cut2 . $or . $cut3 . $or . $cut4 . $or . $cut5 . $or . $cut6 . ' ORDER BY best_ra ASC';
 
+
+        // echo "Query Statement:<BR>";
+        // echo $queryStatement;
+        // echo "<BR>";
 
         // Query the database
-        $results = $db->query($queryStatement);
+        // $results = $db->query($queryStatement);
+        $retval = mysql_query($queryStatement, $conn);
+
+        if(! $retval ) {
+            die('Could not get data: ' . mysql_error());
+        }
 
         // Create an array to store the results
         $data = array();
 
         // Loop through each row and create an associative array (i.e. dictionary) where the column name is the key
-        while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
+        while($row = mysql_fetch_array($retval, MYSQL_ASSOC)) {
             $data[] = $row;
         }  
 
         // Encode the PHP associative array into a JSON associative array
         echo json_encode($data);
-
     }
 
     // Return timebin data
@@ -83,19 +111,19 @@
         // echo "<BR>";
 
         // Query the database
-        $results = $db->query($queryStatement);
+        // $results = $db->query($queryStatement);
+        $retval = mysql_query($queryStatement, $conn);
 
         // Create an array to store the results
         $data = array();
 
         // Loop through each row and create an associative array (i.e. dictionary) where the column name is the key
-        while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
+        while($row = mysql_fetch_array($retval, MYSQL_ASSOC)) {
             $data[] = $row;
         }  
 
         // Encode the PHP associative array into a JSON associative array
         echo json_encode($data);
-
     }
 
     // Return timebin data
@@ -113,15 +141,15 @@
             // Construct the query statement 
             if ($threshold === '6Sigma') {
 
-                $cut1 = '(week == ' . $week . ' and (cast(he_sigma as float)>6) and (cast(sundist as float)>10))';
-                $cut2 = '(week == ' . $week . '  and (cast(sigma as float)>6) and (cast(sundist as float)>10))';
-                $cut3 = '(week == ' . $week . '  and ((cast(sigma as float)>4) and (cast(he_sigma as float)>4) and (cast(sundist as float)>10)) and ((cast(sigma as float)<=6) or (cast(he_sigma as float)<=6)))';
+                $cut1 = '(week = ' . $week . ' and (he_sigma>6) and (sundist>10))';
+                $cut2 = '(week = ' . $week . '  and (sigma>6) and (sundist>10))';
+                $cut3 = '(week = ' . $week . '  and ((sigma>4) and (he_sigma>4) and (sundist>10)) and ((sigma<=6) or (he_sigma<=6)))';
 
-                $cut4 = '(week == ' . $week . '  and ((cast(he_ts as float)>39) and (cast(he_sundist as float)>10)) and ((cast(he_contflag as float)==0) or (cast(he_contflag as float)==1)))';
-                $cut5 = '(week == ' . $week . '  and ((cast(le_ts as float)>39) and (cast(le_sundist as float)>10)) and ((cast(le_contflag as float)==0) or (cast(le_contflag as float)==1)))';
-                $cut6 = '(week == ' . $week . '  and ((cast(le_ts as float)>18) and (cast(he_ts as float)>18)) and ((cast(le_sundist as float)>10) and (cast(he_sundist as float)>10)) and ((cast(le_contflag as float)==0) or (cast(le_contflag as float)==1)) and ((cast(he_contflag as float)==0) or (cast(he_contflag as float)==1)) and (cast(he_le_dist as float)<1.5) and ((cast(he_ts as float)<=39) or (cast(le_ts as float)<=39)))';
+                $cut4 = '(week = ' . $week . '  and ((he_ts>39) and (he_sundist>10)) and ((he_contflag=0) or (he_contflag=1)))';
+                $cut5 = '(week = ' . $week . '  and ((le_ts>39) and (le_sundist>10)) and ((le_contflag=0) or (le_contflag=1)))';
+                $cut6 = '(week = ' . $week . '  and ((le_ts>18) and (he_ts>18)) and ((le_sundist>10) and (he_sundist>10)) and ((le_contflag=0) or (le_contflag=1)) and ((he_contflag=0) or (he_contflag=1)) and (he_le_dist<1.5) and ((he_ts<=39) or (le_ts<=39)))';
 
-                $queryStatement = 'SELECT flareID, cast(num as int), best_ra, best_dec, best_r95, bestPositionSource, fava_ra, fava_dec, lbin, bbin, gall, galb, tmin, tmax, sigma, avnev, nev, he_nev, he_avnev, he_sigma, sundist, varindex, favasrc, fglassoc, assoc, le_ts, le_tssigma, le_ra, le_dec, le_gall, le_galb, le_r95, le_contflag, le_sundist, le_dist2bb, le_ffsigma, le_hightsfrac, le_gtlts, le_flux, le_fuxerr, le_index, le_indexerr, he_ts, he_tssigma, he_ra, he_dec, he_gall, he_galb, he_r95, he_contflag, he_sundist, he_dist2bb, he_ffsigma, he_hightsfrac, he_le_dist, he_gtlts, he_flux, he_fuxerr, he_index, he_indexerr, week, dateStart, dateStop from flares WHERE ' . $cut1 . $or . $cut2 . $or . $cut3 . $or . $cut4 . $or . $cut5 . $or . $cut6 . ' ORDER BY cast(num as float) ASC';
+                $queryStatement = 'SELECT flareID, num, best_ra, best_dec, best_r95, bestPositionSource, fava_ra, fava_dec, lbin, bbin, gall, galb, tmin, tmax, sigma, avnev, nev, he_nev, he_avnev, he_sigma, sundist, varindex, favasrc, fglassoc, assoc, le_ts, le_tssigma, le_ra, le_dec, le_gall, le_galb, le_r95, le_contflag, le_sundist, le_dist2bb, le_ffsigma, le_hightsfrac, le_gtlts, le_flux, le_fuxerr, le_index, le_indexerr, he_ts, he_tssigma, he_ra, he_dec, he_gall, he_galb, he_r95, he_contflag, he_sundist, he_dist2bb, he_ffsigma, he_hightsfrac, he_le_dist, he_gtlts, he_flux, he_fuxerr, he_index, he_indexerr, week, dateStart, dateStop from flares WHERE ' . $cut1 . $or . $cut2 . $or . $cut3 . $or . $cut4 . $or . $cut5 . $or . $cut6 . ' ORDER BY num ASC';
 
                 // echo "Query Statement:<BR>";
                 // echo $queryStatement;
@@ -129,7 +157,7 @@
 
             } else {
 
-                $queryStatement = 'SELECT flareID, cast(num as int), best_ra, best_dec, best_r95, bestPositionSource, fava_ra, fava_dec, lbin, bbin, gall, galb, tmin, tmax, sigma, avnev, nev, he_nev, he_avnev, he_sigma, sundist, varindex, favasrc, fglassoc, assoc, le_ts, le_tssigma, le_ra, le_dec, le_gall, le_galb, le_r95, le_contflag, le_sundist, le_dist2bb, le_ffsigma, le_hightsfrac, le_gtlts, le_flux, le_fuxerr, le_index, le_indexerr, he_ts, he_tssigma, he_ra, he_dec, he_gall, he_galb, he_r95, he_contflag, he_sundist, he_dist2bb, he_ffsigma, he_hightsfrac, he_le_dist, he_gtlts, he_flux, he_fuxerr, he_index, he_indexerr, week, dateStart, dateStop from flares WHERE week == ' . $week . ' ORDER BY cast(num as float) ASC';
+                $queryStatement = 'SELECT flareID, num, best_ra, best_dec, best_r95, bestPositionSource, fava_ra, fava_dec, lbin, bbin, gall, galb, tmin, tmax, sigma, avnev, nev, he_nev, he_avnev, he_sigma, sundist, varindex, favasrc, fglassoc, assoc, le_ts, le_tssigma, le_ra, le_dec, le_gall, le_galb, le_r95, le_contflag, le_sundist, le_dist2bb, le_ffsigma, le_hightsfrac, le_gtlts, le_flux, le_fuxerr, le_index, le_indexerr, he_ts, he_tssigma, he_ra, he_dec, he_gall, he_galb, he_r95, he_contflag, he_sundist, he_dist2bb, he_ffsigma, he_hightsfrac, he_le_dist, he_gtlts, he_flux, he_fuxerr, he_index, he_indexerr, week, dateStart, dateStop from flares WHERE week == ' . $week . ' ORDER BY num ASC';
 
                 // echo "Query Statement:<BR>";
                 // echo $queryStatement;
@@ -138,13 +166,14 @@
             }
             
             // Query the database
-            $results = $db->query($queryStatement);
+            // $results = $db->query($queryStatement);
+            $retval = mysql_query($queryStatement, $conn);
 
             // Create an array to store the results
             $data = array();
 
             // Loop through each row and create an associative array (i.e. dictionary) where the column name is the key
-            while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
+            while($row = mysql_fetch_array($retval, MYSQL_ASSOC)) {
                 $data[] = $row;
             }  
 
@@ -152,7 +181,6 @@
             echo json_encode($data);
 
         }
-
     }
 
     // Return timebin data
@@ -169,20 +197,20 @@
             $or = ' or ';
 
             // Construct the query statement 
-            $queryStatement = 'SELECT * from flares WHERE flareID == "' . $flareID . '"';
+            $queryStatement = "SELECT * from flares WHERE flareID = '" . $flareID . "'";
 
             // echo "Query Statement:<BR>";
             // echo $queryStatement;
             // echo "<BR>";
 
             // Query the database
-            $results = $db->query($queryStatement);
+            $retval = mysql_query($queryStatement, $conn);
 
             // Create an array to store the results
             $data = array();
 
             // Loop through each row and create an associative array (i.e. dictionary) where the column name is the key
-            while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
+            while($row = mysql_fetch_array($retval, MYSQL_ASSOC)) {
                 $data[] = $row;
             }  
 
@@ -190,7 +218,6 @@
             echo json_encode($data);
 
         }
-
     }
 
     // Return basic information on all sources to be displayed in the map
@@ -211,13 +238,13 @@
             // Construct the query statement 
             if ($threshold === '6Sigma') {
 
-                $cut1 = '((cast(he_sigma as float)>6) and (cast(sundist as float)>10) )';
-                $cut2 = '((cast(sigma as float)>6) and (cast(sundist as float)>10) )';
-                $cut3 = '(((cast(sigma as float)>4) and (cast(he_sigma as float)>4) and (cast(sundist as float)>10)) and ((cast(sigma as float)<=6) or (cast(he_sigma as float)<=6)))';
+                $cut1 = '((he_sigma>6) and (sundist>10) )';
+                $cut2 = '((sigma>6) and (sundist>10) )';
+                $cut3 = '(((sigma>4) and (he_sigma>4) and (sundist>10)) and ((sigma<=6) or (he_sigma<=6)))';
 
-                $cut4 = '(((cast(he_ts as float)>39) and (cast(he_sundist as float)>10)) and ((cast(he_contflag as float)==0) or (cast(he_contflag as float)==1)))';
-                $cut5 = '(((cast(le_ts as float)>39) and (cast(le_sundist as float)>10)) and ((cast(le_contflag as float)==0) or (cast(le_contflag as float)==1)))';
-                $cut6 = '(((cast(le_ts as float)>18) and (cast(he_ts as float)>18)) and ((cast(le_sundist as float)>10) and (cast(he_sundist as float)>10)) and ((cast(le_contflag as float)==0) or (cast(le_contflag as float)==1)) and ((cast(he_contflag as float)==0) or (cast(he_contflag as float)==1)) and (cast(he_le_dist as float)<1.5) and ((cast(he_ts as float)<=39) or (cast(le_ts as float)<=39)))';
+                $cut4 = '(((he_ts>39) and (he_sundist>10)) and ((he_contflag=0) or (he_contflag=1)))';
+                $cut5 = '(((le_ts>39) and (le_sundist>10)) and ((le_contflag=0) or (le_contflag=1)))';
+                $cut6 = '(((le_ts>18) and (he_ts>18)) and ((le_sundist>10) and (he_sundist>10)) and ((le_contflag=0) or (le_contflag=1)) and ((he_contflag=0) or (he_contflag=1)) and (he_le_dist<1.5) and ((he_ts<=39) or (le_ts<=39)))';
 
                 $queryStatement = 'SELECT * from flares WHERE ' . $cut1 . $or . $cut2 . $or . $cut3 . $or . $cut4 . $or . $cut5 . $or . $cut6;
 
@@ -237,13 +264,14 @@
             }
 
             // Query the database
-            $results = $db->query($queryStatement);
+            // $results = $db->query($queryStatement);
+            $retval = mysql_query($queryStatement, $conn);
 
             // Create an array to store the results
             $data = array();
             $count = 0;
 
-            while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
+            while($row = mysql_fetch_array($retval, MYSQL_ASSOC)) {
 
                 // Get the ra and dec of each source
                 $raSource = $row['fava_ra'];
@@ -259,16 +287,12 @@
 
                 }
 
-
-
-
             }  
 
             // Encode the PHP associative array into a JSON associative array
             echo json_encode($data);
 
         } 
-
     }
 
     if ($typeOfRequest === 'FlareList') { 
@@ -278,13 +302,13 @@
             $or = ' or ';
 
             // Construct the cut parameters
-            $cut1 = '(cast(he_sigma as float)>6) and (cast(sundist as float)>10)';
-            $cut2 = '(cast(sigma as float)>6) and (cast(sundist as float)>10)';
-            $cut3 = '((cast(sigma as float)>4) and (cast(he_sigma as float)>4) and (cast(sundist as float)>10)) and ((cast(sigma as float)<=6) or (cast(he_sigma as float)<=6))';
+            $cut1 = '(he_sigma>6) and (sundist>10)';
+            $cut2 = '(sigma>6) and (sundist>10)';
+            $cut3 = '((sigma>4) and (he_sigma>4) and (sundist>10)) and ((sigma<=6) or (he_sigma<=6))';
 
-            $cut4 = '((cast(he_ts as float)>39) and (cast(he_sundist as float)>10)) and ((cast(he_contflag as float)==0) or (cast(he_contflag as float)==1))';
-            $cut5 = '((cast(le_ts as float)>39) and (cast(le_sundist as float)>10)) and ((cast(le_contflag as float)==0) or (cast(le_contflag as float)==1))';
-            $cut6 = '((cast(le_ts as float)>18) and (cast(he_ts as float)>18)) and ((cast(le_sundist as float)>10) and (cast(he_sundist as float)>10)) and ((cast(le_contflag as float)==0) or (cast(le_contflag as float)==1)) and ((cast(he_contflag as float)==0) or (cast(he_contflag as float)==1)) and (cast(he_le_dist as float)<1.5) and ((cast(he_ts as float)<=39) or (cast(le_ts as float)<=39))';
+            $cut4 = '((he_ts>39) and (he_sundist>10)) and ((he_contflag=0) or (he_contflag=1))';
+            $cut5 = '((le_ts>39) and (le_sundist>10)) and ((le_contflag=0) or (le_contflag=1))';
+            $cut6 = '((le_ts>18) and (he_ts>18)) and ((le_sundist>10) and (he_sundist>10)) and ((le_contflag=0) or (le_contflag=1)) and ((he_contflag=0) or (he_contflag=1)) and (he_le_dist<1.5) and ((he_ts<=39) or (le_ts<=39))';
 
            // Construct the query statement 
             $queryStatement = 'SELECT flareID, best_ra, best_dec, gall, galb, sigma, fglassoc from flares WHERE ' . $cut1 . $or . $cut2 . $or . $cut3 . $or . $cut4 . $or . $cut5 . $or . $cut6;
@@ -294,19 +318,18 @@
             // echo "<BR>";
 
             // Query the database
-            $results = $db->query($queryStatement);
+            $retval = mysql_query($queryStatement, $conn);
 
             // Create an array to store the results
             $data = array();
 
             // Loop through each row and create an associative array (i.e. dictionary) where the column name is the key
-            while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
+            while($row = mysql_fetch_array($retval, MYSQL_ASSOC)) {
                 $data[] = $row;
             }  
 
             // Encode the PHP associative array into a JSON associative array
             echo json_encode($data);
-
     }
 
 ?>  
